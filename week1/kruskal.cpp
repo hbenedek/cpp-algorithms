84760e85971c6569500f5192079f86b4980b1ac6
Benedek Harsanyi <hben.0204@gmail.com>
2022-02-25 15:57:37 +0100

    add edgelist representation

--- week1/kruskal.cpp
+++ week1/kruskal.cpp
@@ -4,14 +4,19 @@
 #include<list>
 using namespace std;
 
+struct Edge {
+    int src, dest, weight;
+};
+
 class Graph{
 public:    
     int N;
     list<pair<int,int>> *adjacency_list;
-
+    vector<Edge> *edge_list;
     // Constructor
     Graph(int v){
         adjacency_list = new list<pair<int,int>> [v];
+        edge_list = new vector<Edge>;
         N = v;
     }
 
@@ -19,6 +24,18 @@ public:
         adjacency_list[u].push_back(make_pair(v,weight));
         adjacency_list[v].push_back(make_pair(v,weight));
     }
+
+    void build_from_edgelist(vector<Edge> &edges){
+        for (auto &edge: edges)
+        {
+            int src = edge.src;
+            int dest = edge.dest;
+            int weight = edge.weight;
+            adjacency_list[src].push_back(make_pair(dest, weight));
+            adjacency_list[dest].push_back(make_pair(src, weight));
+        }    
+    }
+    
     void print_graph(){
         for (int i = 0; i < N; i++)
         {
@@ -32,16 +49,23 @@ public:
     }
 };
 
-void kruskal(Graph g){}
+void kruskal(Graph g){
+    // sort edges by weights
+
+    // initialize spaning tree
+    //iterate edges and build the tree
+};
 
 int main(){
-    Graph g(5);
-    g.add_edge(1, 2, 0);
-    g.add_edge(4, 2, 0);
-    g.add_edge(1, 3, 0);
-    g.add_edge(4, 3, 0);
-    g.add_edge(1, 4, 0);
-
+    Graph g(6);
+    //g.add_edge(1, 2, 0);
+    //g.add_edge(4, 2, 0);
+    //g.add_edge(1, 3, 0);
+    //g.add_edge(4, 3, 0);
+    //g.add_edge(1, 4, 0);
+    vector<Edge> edges ={{0, 1, 6}, {1, 2, 7}, {2, 0, 5}, {2, 1, 4}, {3, 2, 10}, {5, 4, 1}, {4, 5, 3}};
+      
+    g.build_from_edgelist(edges);  
     g.print_graph();
     return 0;  
 }
